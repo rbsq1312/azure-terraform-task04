4ba721d9b5d43fa6caad35ff8c86d6b357d2aa6a
Theodor Laurentiu Robescu <theodor-laurentiu_robescu@epam.com>
2025-04-03 19:22:37 +0000

    Fix Terraform formatting

--- task04/main.tf
+++ task04/main.tf
@@ -107,12 +107,12 @@ resource "azurerm_network_interface_security_group_association" "nic_nsg_associa
 
 # Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
-  admin_username      = var.vm_username
-  admin_password      = var.vm_password
+  name                            = var.vm_name
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = var.vm_size
+  admin_username                  = var.vm_username
+  admin_password                  = var.vm_password
   disable_password_authentication = false
 
   network_interface_ids = [
@@ -124,7 +124,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
 
-      source_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-noble"
     sku       = "24_04-lts"
