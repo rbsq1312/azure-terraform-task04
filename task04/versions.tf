64b02873488cf17f82731103b6f46496c08a0475
Theodor Laurentiu Robescu <theodor-laurentiu_robescu@epam.com>
2025-04-03 19:41:25 +0000

    Fix AzureRM provider version format

--- task04/versions.tf
+++ task04/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.11.0, < 4.0.0"
+      version = "~> 3.11.0"
     }
   }
   required_version = ">= 1.5.7"
